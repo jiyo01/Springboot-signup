e98e2fa248f8e8d4f174d48337a4b7e12f95e1ed
jiyo01 <jyyy0219@gmail.com>
2023-01-25 19:44:41 +0900

    프로젝트 생성

---
+++ Springboot-signup/src/main/java/com/signup/SpringbootSignupApplication.java
@@ -0,0 +1,13 @@
+package com.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSignupApplication.class, args);
+	}
+
+}
